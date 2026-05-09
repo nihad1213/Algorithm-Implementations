f6137ee59bb39af631788025b90bc80f9a2f8651
nihad1213 <nihad.nemetli@gmail.com>
2026-05-09 21:08:03 +0400

    new: Js Solution created

--- Searching/linear-search/main.js
+++ Searching/linear-search/main.js
@@ -1,16 +1,30 @@
 /**
+ * Linear search, or sequential search,
+ * is the simplest method for finding a
+ * specific value within a collection of data.
  * 
- * @param {*} arr 
- * @param {*} target 
+ * param {Array} arr - the array to search through
+ * param {mixed} target - the value to search for
+ * return {number} The index of the target value, or -1 if not found
+ * 
+ * Best Case: O(1) - when the target is the first element
+ * Worst Case: O(n) - when the target is the last element
+ * Average Case: O(n) - when the target is somewhere in the middle
+ * Space Complexity: O(1) - no additional space is used 
  */
 
-
 function linearSearch(arr, target) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === target) {
+            return i;
+        }
+    }
 
+    return -1;
 }
 
-console.log(linearSearch([1, 2, 3, 4, 5], 3) + "\n");
-console.log(linearSearch([1, 2, 3, 4, 5], 6) + "\n");
-console.log(linearSearch([], 1)              + "\n");
-console.log(linearSearch([1, 2, 3, 4, 5], 1) + "\n");
-console.log(linearSearch([1, 2, 3, 4, 5], 5) + "\n");
+console.log(linearSearch([1, 2, 3, 4, 5], 3));
+console.log(linearSearch([1, 2, 3, 4, 5], 6));
+console.log(linearSearch([], 1));
+console.log(linearSearch([1, 2, 3, 4, 5], 1));
+console.log(linearSearch([1, 2, 3, 4, 5], 5));
